dc1dea2b74d7eafcc63bf6f81ad0c3de5ff13dc7
TiagoM13 <thyagomotha2000@gmail.com>
2024-11-27 11:58:32 -0300

    feat: add validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './infra/http/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   await app.listen(3000);
 }
 bootstrap();
