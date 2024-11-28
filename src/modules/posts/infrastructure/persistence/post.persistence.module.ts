f3e96cfca1908ee6fa86973abd9e3f46e905f769
TiagoM13 <thyagomotha2000@gmail.com>
2024-11-28 11:32:33 -0300

    feat: remove files

--- src/modules/posts/infrastructure/persistence/post.persistence.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { PostRepository } from '../../domain/repositories/post.repository';
-
-@Module({
-  providers: [PrismaService, PostRepository],
-  exports: [PostRepository],
-})
-export class PostPersistenceModule {}
